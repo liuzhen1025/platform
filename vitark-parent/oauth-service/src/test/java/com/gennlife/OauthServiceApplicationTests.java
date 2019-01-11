b48224fe22e33b20747b1d6fd9eb4f25a9efab71
刘朕 <liuzhen@gennlife.com>
2019-01-11 15:04:23 +0800

    添加banner 文件

--- vitark-parent/oauth-service/src/test/java/com/gennlife/OauthServiceApplicationTests.java
+++ vitark-parent/oauth-service/src/test/java/com/gennlife/OauthServiceApplicationTests.java
@@ -2,15 +2,18 @@ package com.gennlife;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OauthServiceApplicationTests {
-
+    private static Logger logger = LoggerFactory.getLogger(OauthServiceApplicationTests.class);
 	@Test
 	public void contextLoads() {
+        logger.info("start test application");
 	}
 
 }
